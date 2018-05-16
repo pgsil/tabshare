8fd831486d1cb2bb1e5389ad8fba6176205e04c5
pgsil <email@email.com>
2018-05-15 21:06:45 -0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-### tabshare
-
-tabshare is a simple proof of concept app that aims to help split the bill between two or more people.
--- src/components/CustomerPays.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const CustomerPays = ({ customerName, price }) => (
-  <p>
-    <b>{customerName}</b> pays R$ <b>{price.replace(".", ",")}</b>
-  </p>
-);
-
-export default CustomerPays;
